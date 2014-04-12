3d5d7fab5b21a3df0e5312d0a32eb46b7a3ef48f
iryanclarke <clarke_93@hotmail.com>
2014-04-11 22:34:12 -0400

    Addition of Owl ;)

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- 
+       Step 2: Draw the rest of the fucking owl! 
+                   /\_______/\
+                   \ `-'"'-' /
+                   / \_   _/ \
+                   |  d\_/b  |
+                  .'\   V   /'.
+                 /   '-...-'   \
+                |  /         \  |
+                 \/\         /\/
+              ooo==(||)-=-(||)==
+
+
+ --> 
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
       <meta charset="utf-8" />
@@ -24,7 +38,7 @@
            <div class="content">
                 <h1>Hi,</h1>
                 <h2>I'm Ian!&nbsp;&nbsp;&nbsp;(That's me! ...sorta --->)</h2>
-                <p>I'm a Front-End Web developer with a passion for clean, efficient code and an eye for design. </p>
+                <p>I'm a Front-End Web developer with a passion for clean, efficient code and an eye for design </p>
                 <p>&nbsp;</p>
                 <p>&nbsp;</p>
                 <p>&nbsp;</p>
@@ -40,10 +54,6 @@
                 <p>Pellentesque egestas dictum porta. Pellentesque tempor, justo eu convallis placerat, nulla nibh venenatis metus, eget venenatis metus mi nec neque. Nunc sed hendrerit diam, ut varius ligula. Pellentesque suscipit dui eget interdum consectetur. Nullam tortor ligula, lobortis quis dictum et, bibendum ut enim. Aliquam est metus, posuere et sapien id, condimentum mollis tortor. Suspendisse vulputate viverra justo. Donec in ultricies magna. Aenean pretium bibendum semper. Pellentesque eget nisi lobortis, condimentum eros eget, ullamcorper nisl. Sed nec felis id lectus bibendum ornare.</p>
                 <p>Nulla viverra, erat sit amet pellentesque facilisis, urna ligula consectetur eros, at laoreet purus sem non dolor. Proin suscipit est aliquet velit pulvinar, vel vulputate erat mollis. Duis nec lectus ultricies, volutpat enim dictum, ullamcorper tortor. Fusce vel est non ipsum ultricies cursus. Sed id risus non lacus dapibus tempor a sed odio. Donec non rhoncus tortor. Donec pharetra ut dolor eu accumsan. Nunc mi lacus, vulputate vitae placerat id, aliquam ut mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Aenean tortor erat, pharetra in lorem sed, ultrices pellentesque est. Vivamus faucibus, erat eleifend dapibus commodo, sem odio pharetra augue, at tempor quam purus quis leo. Aliquam sed est vitae leo cursus mattis. Proin vel nulla sit amet augue posuere rhoncus.</p>
                 <p>Content is coming soon! :)</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
             </div>
         </div> 
       </section>   
@@ -56,10 +66,6 @@
                 <p>Pellentesque egestas dictum porta. Pellentesque tempor, justo eu convallis placerat, nulla nibh venenatis metus, eget venenatis metus mi nec neque. Nunc sed hendrerit diam, ut varius ligula. Pellentesque suscipit dui eget interdum consectetur. Nullam tortor ligula, lobortis quis dictum et, bibendum ut enim. Aliquam est metus, posuere et sapien id, condimentum mollis tortor. Suspendisse vulputate viverra justo. Donec in ultricies magna. Aenean pretium bibendum semper. Pellentesque eget nisi lobortis, condimentum eros eget, ullamcorper nisl. Sed nec felis id lectus bibendum ornare.</p>
                 <p>Nulla viverra, erat sit amet pellentesque facilisis, urna ligula consectetur eros, at laoreet purus sem non dolor. Proin suscipit est aliquet velit pulvinar, vel vulputate erat mollis. Duis nec lectus ultricies, volutpat enim dictum, ullamcorper tortor. Fusce vel est non ipsum ultricies cursus. Sed id risus non lacus dapibus tempor a sed odio. Donec non rhoncus tortor. Donec pharetra ut dolor eu accumsan. Nunc mi lacus, vulputate vitae placerat id, aliquam ut mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Aenean tortor erat, pharetra in lorem sed, ultrices pellentesque est. Vivamus faucibus, erat eleifend dapibus commodo, sem odio pharetra augue, at tempor quam purus quis leo. Aliquam sed est vitae leo cursus mattis. Proin vel nulla sit amet augue posuere rhoncus.</p>
                 <p>Content is coming soon! :)</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
             </div>
         </div>    
       </section>   
@@ -72,10 +78,6 @@
                 <p>Pellentesque egestas dictum porta. Pellentesque tempor, justo eu convallis placerat, nulla nibh venenatis metus, eget venenatis metus mi nec neque. Nunc sed hendrerit diam, ut varius ligula. Pellentesque suscipit dui eget interdum consectetur. Nullam tortor ligula, lobortis quis dictum et, bibendum ut enim. Aliquam est metus, posuere et sapien id, condimentum mollis tortor. Suspendisse vulputate viverra justo. Donec in ultricies magna. Aenean pretium bibendum semper. Pellentesque eget nisi lobortis, condimentum eros eget, ullamcorper nisl. Sed nec felis id lectus bibendum ornare.</p>
                 <p>Nulla viverra, erat sit amet pellentesque facilisis, urna ligula consectetur eros, at laoreet purus sem non dolor. Proin suscipit est aliquet velit pulvinar, vel vulputate erat mollis. Duis nec lectus ultricies, volutpat enim dictum, ullamcorper tortor. Fusce vel est non ipsum ultricies cursus. Sed id risus non lacus dapibus tempor a sed odio. Donec non rhoncus tortor. Donec pharetra ut dolor eu accumsan. Nunc mi lacus, vulputate vitae placerat id, aliquam ut mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Aenean tortor erat, pharetra in lorem sed, ultrices pellentesque est. Vivamus faucibus, erat eleifend dapibus commodo, sem odio pharetra augue, at tempor quam purus quis leo. Aliquam sed est vitae leo cursus mattis. Proin vel nulla sit amet augue posuere rhoncus.</p>
                 <p>Content is coming soon! :)</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
             </div>
         </div>    
       </section>   
